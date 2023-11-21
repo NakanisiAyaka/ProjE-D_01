59c6c14284c1b57adc7d63cf695f8529175e548f
nakanishi <c0b221119f@edu.teu.ac.jp>
2023-11-21 15:45:05 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -6,20 +6,20 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#1
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#練習１
     tmr = 0
-    kk_img = pg.image.load("ex01/fig/3.png")#2gazou
-    kk_img = pg.transform.flip(kk_img,True,False)#2hanten
-    kk_img = [kk_img,pg.transform.rotozoom(kk_img,10,1.0)]#3
+    kk_img = pg.image.load("ex01/fig/3.png")#練習２
+    kk_img = pg.transform.flip(kk_img,True,False)#練習２
+    kk_img = [kk_img,pg.transform.rotozoom(kk_img,10,1.0)]#練習３
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])#4
-        screen.blit(kk_img[1], [300,200]) 
+        screen.blit(bg_img, [0, 0])#練習４
+        screen.blit(kk_img[tmr%2], [300,200]) 
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(10)
 
 
 if __name__ == "__main__":
